7d8748db001ef46242921cd900e61f716aa79b89
Niclas <147914782+GarNicWahr@users.noreply.github.com>
2024-07-02 21:31:13 +0200

    Animation und Item Skript

--- GameJamTemplate/Assets/Scripts/Miscellaneous/Item.cs
+++ GameJamTemplate/Assets/Scripts/Miscellaneous/Item.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
 
-    public int itemValue = 1;
     public int itemType = 0;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerInventory>() != null)
         {
-            other.GetComponent<PlayerInventory>().ItemCollected(itemValue,itemType);
+            other.GetComponent<PlayerInventory>().ItemCollected(itemType);
             Destroy(gameObject);
         }
     }
