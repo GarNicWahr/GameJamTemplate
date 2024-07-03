1a9b5c1744ea01a613a62b52cd88a375ead8cb7e
MafackerJones <lust20035@hs-ansbach.de>
2024-07-04 00:50:00 +0200

    Added Doors
    - Added Animations
    - Added Logic

---
+++ GameJamTemplate/Assets/Scripts/Miscellaneous/OpenDoor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetBool("isOpen", false);
+        }
+    }
+}
