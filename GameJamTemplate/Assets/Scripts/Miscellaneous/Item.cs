9d3727df3dd4901706fedfca246f34b2ada8a1f8
Niclas <147914782+GarNicWahr@users.noreply.github.com>
2024-07-03 20:30:53 +0200

    Particle Systems

--- GameJamTemplate/Assets/Scripts/Miscellaneous/Item.cs
+++ GameJamTemplate/Assets/Scripts/Miscellaneous/Item.cs
@@ -5,11 +5,16 @@ public class Item : MonoBehaviour
 
     public int itemType = 0;
 
+    public GameObject ParticleSplash;
+
+    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerInventory>() != null)
         {
             other.GetComponent<PlayerInventory>().ItemCollected(itemType);
+            Instantiate(ParticleSplash, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
